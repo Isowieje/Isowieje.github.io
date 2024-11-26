a56eb5e421bfadfc9661822b208ee4ce166bb58a
Isowieje <gehanghaly151@gmail.com>
2024-11-25 21:44:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        showSlide(currentSlide + 1);
+    } else if (event.key === 'ArrowLeft') {
+        showSlide(currentSlide - 1);
+    }
+});
+
+// Initial setup
+slides[currentSlide].classList.add('active');
